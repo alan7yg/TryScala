2524229df1055c181d1ef1328a012978d6fd0fd9
kaihaosw <kaihaosw@gmail.com>
2015-10-19 15:06:36 +0800

    add Horse and GameStation

---
+++ DistributedRacingHorse/src/main/scala/com/kaihaosw/local/GameStationActor.scala
@@ -0,0 +1,39 @@
+package com.kaihaosw.local
+
+import akka.actor.Actor
+import akka.actor.ActorRef
+import akka.actor.PoisonPill
+import akka.actor.Props
+import akka.routing.BroadcastRouter
+
+object GameStation {
+  case object Start
+}
+
+class GameStationActor extends Actor {
+  import Horse._
+
+  val horseNumber = 5
+  val trackLength = 100
+  var observeCount = 0
+
+  val horseActorVector: Vector[ActorRef] =
+    (1 to horseNumber).map(n => context.actorOf(Props(new HorseActor(n.toString)), "horse-" + n)).toVector
+
+  val broadcastHorses = context.actorOf(Props.empty.withRouter(BroadcastRouter(routees = horseActorVector)))
+
+  def receive: Receive = {
+    case o: Observe =>
+      observeCount += 1
+      if (o.trackLength >= trackLength) {
+        broadcastHorses ! PoisonPill
+        println(o.id + " wins")
+        context.system.shutdown
+      }
+      if (observeCount == horseNumber) {
+        observeCount = 0
+        println()
+        broadcastHorses ! Run
+      }
+  }
+}
---
+++ DistributedRacingHorse/src/main/scala/com/kaihaosw/local/HorseActor.scala
@@ -0,0 +1,30 @@
+package com.kaihaosw.local
+
+import akka.actor.Actor
+import scala.util.Random
+
+object Horse {
+  case object Run
+  case class Observe(id: String, trackLength: Int)
+}
+
+class HorseActor(id: String) extends Actor {
+  import Horse._
+
+  var track = ""
+  val random = new Random()
+  def randomStep = random.nextInt(3)
+
+  def run(): Unit = {
+    track += "*" * randomStep
+    println(track)
+  }
+
+  override def toString: String = s"Horse '$id' '$track'"
+
+  def receive: Receive = {
+    case Run =>
+      run()
+      sender ! Observe(id, track.length)
+  }
+}
